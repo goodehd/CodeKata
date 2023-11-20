79959400e95c9e9121066dc945b0eeef20d860bc
goodehd <75984454+goodehd@users.noreply.github.com>
2023-11-20 09:19:23 +0900

    서울에서 김서방 찾기

---
+++ Lv02/14.cpp
@@ -0,0 +1,21 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int solution(int num) {
+//    int answer = 0;
+//    long long input = num;
+//    while (input > 1) {
+//        if (input % 2 == 0)
+//            input /= 2;
+//        else
+//            input = input * 3 + 1;
+//
+//        answer++;
+//
+//        if (answer == 500)
+//            break;
+//    }
+//    return answer == 500 ? -1 : answer;
+//}
