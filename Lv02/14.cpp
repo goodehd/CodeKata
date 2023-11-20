d3bf41dc34860f45e9dc8477813dc3fede237b52
goodehd <75984454+goodehd@users.noreply.github.com>
2023-11-20 16:27:38 +0900

    서울에서 김서방 찾기

--- Lv02/14.cpp
+++ Lv02/14.cpp
@@ -1,21 +1,13 @@
-//#include <string>
-//#include <vector>
-//
-//using namespace std;
-//
-//int solution(int num) {
-//    int answer = 0;
-//    long long input = num;
-//    while (input > 1) {
-//        if (input % 2 == 0)
-//            input /= 2;
-//        else
-//            input = input * 3 + 1;
-//
-//        answer++;
-//
-//        if (answer == 500)
-//            break;
-//    }
-//    return answer == 500 ? -1 : answer;
-//}
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    string answer = "김서방은 ";
+    for(int i = 0; i < seoul.size(); ++i){
+        if(seoul[i] == "Kim")
+            answer = answer + to_string(i) + "에 있다";
+    }
+    return answer;
+}
